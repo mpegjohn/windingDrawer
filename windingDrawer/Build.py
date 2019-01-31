89109e4e23a860fb0410bc73c0dbc3b064754690
John Wilkinson <john.wilkinson@fotechsolutions.com>
2019-01-31 16:29:00 +0000

    ss

--- windingDrawer/Build.py
+++ windingDrawer/Build.py
@@ -1,10 +1,14 @@
 
-class Winding:
+class Build:
 
     def __init__(self, name):
         self.name = name
         self.sections = []
+	self.wire = {}
 
     def add_section(self, section):
         self.sections.append(section)
+
+    def add_wire(self, name, bare_thickness, maximum_thickness):
+
 
